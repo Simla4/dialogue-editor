c9afbbce0ed34d7e81043d67911d5faa02d7e5bc
Simla Beken <38881186+Simla4@users.noreply.github.com>
2024-03-28 11:58:28 +0300

    import: text mesh pro

---
+++ procudural-dugeon-generation/Assets/Scripts/Editor/Managers/DialogueManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    #region Variables
+
+    private String actorName;
+    private String mainText;
+    private String optionText;
+
+    #endregion
+
+    #region Callbacks
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region OtherMethods
+
+    private String GenerateMainText()
+    {
+        return null; 
+    }
+
+    #endregion
+}
