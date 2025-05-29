d78a0b250d2ecb4176348261e1435677007febe3
Simla4 <simlabeken4@gmail.com>
2025-05-29 16:26:27 +0300

    (dialogue-loader) feat: closed text UI when dialogue finishes

--- Assets/Scripts/Test/RunTime/DialogueSystem/DialogueLoader.cs
+++ Assets/Scripts/Test/RunTime/DialogueSystem/DialogueLoader.cs
@@ -9,31 +9,69 @@ public class DialogueLoader : MonoBehaviour
     [SerializeField] private TextMeshProUGUI dialogueTxt;
     [SerializeField] private Transform dialogueContainer;
 
-    private DialogueNodeGraphSO dialogue;
+    private DialogueNodeGraphSO dialogueNodeGraph;
+    private DialogueNodeSO dialogueNode;
     
-    public static Action OnCallNextDialogueText;
+    public static Action<string> OnCallDialogueData;
+    public static Action OnDialogueEnd;
+
+    #endregion
+
+    #region Callbacks
+
+    private void OnEnable()
+    {
+        OnCallDialogueData += GetNextDialogueText;
+    }
+
+    private void OnDisable()
+    {
+        OnCallDialogueData -= GetNextDialogueText;
+    }
 
     #endregion
 
     #region OtherMethods
 
-    public void LoadDialogueData()
-    { 
-        dialogue = DialogueDataManager.Instance.GetDialogue("FirstDialogue");
-
-        TextMeshProUGUI textBox = InstantiateTextBox();
-        textBox.text = dialogue.dialogueNodeList[0].actorType.actorName + ": "+ dialogue.dialogueNodeList[0].dialogueText;
-    }
-
-    public void GetNextDialogueText()
+    private void LoadDialogueData(string dialogueId)
     {
-        
+        dialogueNodeGraph = DialogueDataManager.Instance.GetDialogue(dialogueId);
+        dialogueNode = dialogueNodeGraph.dialogueNodeList[0];
+        Debug.Log("dialogue node:" + dialogueNodeGraph + " dialogueNode: " + dialogueNode);
+
+        var textbox = InstantiateTextBox(dialogueNode.actorType.actorName, dialogueNode.dialogueText);
+        textbox.name = dialogueNode.id;
     }
 
-    private TextMeshProUGUI InstantiateTextBox()
+    private void GetNextDialogueText(string dialogueId)
+    {
+        var childDialogueList = dialogueNodeGraph.dialogueNodeDictionary[dialogueId].childDialogueList;
+
+        if (childDialogueList.Count <= 0)
+        {
+            OnDialogueEnd?.Invoke();
+            return;
+        }
+        
+        for (int i = 0; i < childDialogueList.Count; i++)
+        {
+            var nextDialogue = dialogueNodeGraph.dialogueNodeDictionary[childDialogueList[i]];
+            var textbox = InstantiateTextBox(nextDialogue.actorType.actorName, nextDialogue.dialogueText);
+            textbox.name = childDialogueList[i];
+        }
+    }
+
+    private TextMeshProUGUI InstantiateTextBox(string actorName, string dialogueText)
     {
         TextMeshProUGUI textBox = Instantiate(dialogueTxt, dialogueContainer);
+        textBox.text = actorName + ": " + dialogueText;
+        return textBox;
+    }
 
+    private TextMeshProUGUI InstantiateTextBox(string dialogueText)
+    {
+        TextMeshProUGUI textBox = Instantiate(dialogueTxt, dialogueContainer);
+        textBox.text =  dialogueText;
         return textBox;
     }
 
