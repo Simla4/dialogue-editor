a71a619882a5fe605c4e1980cf8b080ac194a32d
Simla4 <simlabeken4@gmail.com>
2025-10-04 15:03:17 +0300

    (feat): changing health bar if there is action

--- Assets/Scripts/Test/RunTime/DialogueSystem/DialogueLoader.cs
+++ Assets/Scripts/Test/RunTime/DialogueSystem/DialogueLoader.cs
@@ -20,19 +20,22 @@ public class DialogueLoader : MonoBehaviour
     
     public static Action<string> OnCallDialogueData;
     public static Action OnDialogueEnd;
+    public static Action<ActionTypes, int> OnCallDialogueAction;
 
-    #endregion
+        #endregion
 
-    #region Callbacks
+        #region Callbacks
 
     private void OnEnable()
     {
         OnCallDialogueData += GetNextDialogueText;
+        OnCallDialogueData += CallDialogueAction;
     }
 
     private void OnDisable()
     {
         OnCallDialogueData -= GetNextDialogueText;
+        OnCallDialogueData -= CallDialogueAction;
     }
 
     #endregion
@@ -66,6 +69,17 @@ public class DialogueLoader : MonoBehaviour
             var nextDialogue = dialogueNodeGraph.dialogueNodeDictionary[childDialogueList[i]];
             var textbox = InstantiateTextBox(nextDialogue.actorType.actorName, nextDialogue.dialogueText, nextDialogue.actorType.actorIcon);
             textbox.name = childDialogueList[i];
+        }
+    }
+    
+    
+    private void CallDialogueAction(string dialogueId)
+    {
+        var currentActions = dialogueNodeGraph.dialogueNodeDictionary[dialogueId].actions;
+
+        for (int i = 0; i < currentActions.Count; i++)
+        {
+            OnCallDialogueAction?.Invoke(currentActions[i].actionType, currentActions[i].ActionValue);
         }
     }
 
